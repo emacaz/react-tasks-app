1bee81e9c915027fe65f2811bfb5990097121fa1
Emanuel Castillo <ecastillo76@misena.edu.co>
2023-12-19 12:14:53 +0100

    Adding tasks logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import TaskList from './TaskList'
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
-function HolaMundo() {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle) {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: "nueva tarea"
+    }])
+  }
+
   return (
-    <div>
-      <TaskList />
-    </div>
-  )
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
+    </>
+  );
 }
 
-export default HolaMundo
+export default App;
